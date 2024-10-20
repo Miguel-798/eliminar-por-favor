0119eebd43dd7c518623e0faca9d88a9c7d8ea4c
migueldev <tishimig07@gmail.com>
2024-10-20 02:19:09 -0500

    Mi primer PDiseño Strategy

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
-a = 1
-b = 2
+class IDiscountStrategy():
+    def GetDiscount():
+        pass
 
-def sum(d, e):
-    return d + e 
-print(sum(a, b))
+class VipDiscountStrategy(IDiscountStrategy):
+    def GetDiscount(monto: float):
+        return monto * 0.2
+
+
+
+
+class DiscountService():
+    def __init__(self, vip:IDiscountStrategy):
+        self.discountstrategy = vip
+
+    def calculateDiscount(self, monto:float):
+        return self.discountstrategy.GetDiscount(monto)
+
+a = DiscountService(VipDiscountStrategy)
+print(a.calculateDiscount(1000))
+
+class PremiumDiscountStrategy(IDiscountStrategy):
+    def GetDiscount(monto: float):
+        return monto * 0.1
+
+class RegularDiscountStrategy(IDiscountStrategy):
+    def GetDiscount(monto: float):
+        return monto * 0.05
+
+a = DiscountService(PremiumDiscountStrategy)
+print(a.calculateDiscount(1000))
+
+a = DiscountService(RegularDiscountStrategy)
+print(a.calculateDiscount(1000))
